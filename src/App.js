c8036cd431f89077be96cf044df2b23d6153b0ac
Sibindra <timalsinasibindra@gmail.com>
2022-11-05 15:14:15 +0545

    added components to hero

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
@@ -8,6 +9,7 @@ function App() {
     <>
     <Header/>
     <Hero/>
+    <Footer/>
     </>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,44 @@
 import React from "react";
 
-export default function Hero(){
-    return(
-        <>
-            Hero works
-        </>
-    )
+import article from "../images/article.svg"
+
+export default function Hero() {
+    return (
+        <div id="Hero">
+            {/* first container */}
+            <div id="container1">
+                <div id="about">
+                    <hr id="line" />
+                    <button id="about">About</button>
+                </div>
+            </div>
+
+            {/* text container inside first container*/}
+            <div id="textcontainer">
+                <h1 id="title">
+                    Hi,
+                    <br />I am Sibindra Timalsina
+                </h1>
+
+                <h3 id="caption">Developer and Tech Enthuasist</h3>
+
+                <h5 id="desc">
+                    I make amazing, minimal and clean design and webpages for
+                    you !
+                </h5>
+
+                <div id="buttoncontainer">
+                    <button id="hire">Hire Me</button>
+                </div>
+            </div>
+
+            <div id="container2">
+                <img
+                    src={article}
+                    alt="#"
+                    id="image"
+                />
+            </div>
+        </div>
+    );
 }
