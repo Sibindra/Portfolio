cd8a6da22948cfd217930f7cd9be58d735bfcc40
Sibindra <timalsinasibindra@gmail.com>
2022-11-05 12:13:00 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+import "./components/styles/index.css"
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return <div id="Header">
+        <a href="tel:+9779862903122" id="tel">call me (+977)9862903122</a>
+        <a href="mailto:timalsinasibindra@gmail.com" id="email">timalsinasibindra@gmail</a>
+        <a href="" id="chat">let's chat</a>
+    </div>;
+}
